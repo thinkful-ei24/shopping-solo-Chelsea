27184cddfdb219184e9b0ea09d83f8ca33c2a8ee
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-24 12:29:39 -0400

    We created a generateItemElement function and a generateShoppinItemsString function, as well started the renderShoppingList function

--- index.js
+++ index.js
@@ -8,8 +8,34 @@
 // You should be able to delete items from the list
 
 // render shopping list
+
+const STORE = [
+  {name: 'apple', checked: false},
+  {name: 'cucumber', checked: true}
+];
+
+function generateItemElement (item, itemIndex, template) {
+  return `<li>${item.name}</li>`;
+}
+
+function generateShoppingItemsString (shoppingList) {
+  const items = shoppingList.map((item, index) => {
+    generateItemElement(item, index);
+  });
+  return items.join('');
+}
+
 function renderShoppingList() {
-  console.log('`renderShoppingList` ran');
+  //renders or shows the shopping list in the DOM
+  //for each item in STORE, generate a string representing an <li> with: 
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+  $(".js-shopping-list").html(shoppingListItemsString);
+  //item name rendered as inner text
+  //item's index in the store set as a data attribute for the <li>
+  //item's checked state (true or false) rendered as the presence or absence of a  CSS class for indicating checked items
+  //join together the indivisual item strings into one long string
+  //insert the <li>s string inside the .js-shopping-list <ul> into the DOM
+  
 }
 
 // add items to list
